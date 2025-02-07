7b92468ac01a6755add5f79a365b61e1dfb2dce9
Fernanda Marques <f3rnandamarques@gmail.com>
2025-02-07 18:52:57 -0300

    feat: battle end

--- classes.js
+++ classes.js
@@ -88,6 +88,17 @@ class Monster extends Sprite {
         this.name = name
         this.attacks = attacks
     }
+
+    faint() {
+        document.querySelector('#dialogueBox').innerHTML = this.name + ' fainted!'
+        gsap.to(this.position, {
+            y: this.position.y + 20
+        })
+        gsap.to(this, {
+            opacity: 0
+        })
+    }
+
     attack({ attack, recipient, renderedSprites }) {
         document.querySelector('#dialogueBox').style.display = 'block'
         document.querySelector('#dialogueBox').innerHTML = this.name + ' used ' + attack.name
@@ -98,7 +109,7 @@ class Monster extends Sprite {
         let rotation = 1
         if (this.isEnemy) rotation = -2.2
 
-        this.health -= attack.damage
+        recipient.health -= attack.damage
 
         switch (attack.name) {
             case 'Fireball':
@@ -126,7 +137,7 @@ class Monster extends Sprite {
                     onComplete: () => {
                         // Enemy actually gets hit
                         gsap.to(healthBar, {
-                            width: this.health + '%'
+                            width: recipient.health + '%'
                         })
 
                         gsap.to(recipient.position, {
@@ -162,7 +173,7 @@ class Monster extends Sprite {
                         onComplete: () => {
                             // Enemy actually gets hit
                             gsap.to(healthBar, {
-                                width: this.health + '%'
+                                width: recipient.health + '%'
                             })
 
                             gsap.to(recipient.position, {
